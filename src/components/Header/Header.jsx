02ec48dc14de5f9e0056a15419b9cd10ebafd3f4
ViniciusRodrigues/ <vinyro74@gmail.com>
2025-07-31 09:56:00 -0300

    Alteração no Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import styles from './Header.module.css';
-
+let nomeClient = "Vinicius Rodrigues";
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Controle Financeiro</h1>
-        <p className={styles.subtitle}>Organize seus gastos com simplicidade</p>
+    <header className={`${styles.header} bg-primary`}>
+      <div className={`container pt-lg-5 pt-3 `}>
+        <h1 className={styles.title}>{`Olá,${nomeClient}`}</h1>
+        <p className={`${styles.subtitle} fs-4`}>Seja bem-vindo</p>
       </div>
     </header>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,31 +4,17 @@ import GastosMes from "../components/GastosMes/GastosMes"
 const Home = () => {
 
   const gastosData = [
-    {
-      id: 1,
-      quemGastou: "FDSGHFGDSHFKJ",
-      descricao: "teste1",
-      cartao: "Cartão Lorem",
-      diaPagamento: 2,
-      mesPagamento: "agosto",
-      valor: 10000.00,
-    },
-     {
-      id: 2,
-      quemGastou: "Cleber",
-      descricao: "tete2",
-      cartao: "Cartão Lorem",
-      diaPagamento: 30,
-      mesPagamento: "novembro",
-      valor: 23251.80,
-    },
+
   ];
 
   return (
     <>
       <div className="container">
         <div className="d-flex flex-column gap-5 my-5">
-          <GastosMes gastosData={gastosData} />
+          <div className="d-flex gap-3">
+            <GastosMes gastosData={gastosData} />
+          </div>
+
           <Gastos gastosData={gastosData} />
         </div>
       </div>
