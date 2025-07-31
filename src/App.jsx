8a91d4f4042687b634ea0e84d4410b7c5ed984a1
ViniciusRodrigues/ <vinyro74@gmail.com>
2025-07-31 09:30:40 -0300

    Alteração no Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import Footer from "./components/Footer/Footer"
 import Header from "./components/Header/Header"
 import Home from "./pages/Home"
+import styles from "./App.module.css"
 
 const App = () => {
   return (
-      <>
-        {/* <Header /> */}
+    <>
+      <Header />
+      <div className={styles.Home}>
         <Home />
-        {/* <Footer /> */}
-      </>
+      </div>
+
+      {/* <Footer /> */}
+    </>
   )
 }
 
